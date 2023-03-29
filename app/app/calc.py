1a7acefafab5e53a835af72b410beb630d0bd6b8
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2023-03-30 00:31:38 +0600

    docker compose add psycopg2 and down and rebuild

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+
+"""
+calculator 
+"""
+
+def add(x,y):
+    return x+y
+
+
+def subtract(x,y):
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """test """
+    def test_add_number(self):
+        res= calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+
+    def test_subtract_numbers(self):
+        res=calc.subtract(10,15)
+
+        self.assertEqual(res,5)
